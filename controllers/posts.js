e46c28395b41bc4d0301321964d274ce1fee47aa
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-09 22:17:21 +0900

    Add multiple image upload feature to post resource

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,10 @@
 const Post = require('../models/post');
+const cloudinary = require('cloudinary');
+cloudinary.config({
+    cloud_name: 'kenji',
+    api_key: '387546948933283',
+    api_secret: process.env.CLOUDINARY_SECRET
+});
 
 module.exports = {
     // Posts Index
@@ -12,6 +18,14 @@ module.exports = {
     },
     // Posts Create
     async postCreate(req, res, next) {
+        req.body.post.images = [];
+        for (const file of req.files) {
+            let image = await cloudinary.v2.uploader.upload(file.path);
+            req.body.post.images.push({
+                url: image.secure_url,
+                public_id: image.public_id
+            });
+        }
         let post = await Post.create(req.body.post);
         res.redirect(`/posts/${post.id}`);
     },

5dcd9880c985be0419033d4425e5e48862076137
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-09 15:19:14 +0900

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,59 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
     res.render('index', { title: 'Surf Shop - Home' });
+});
+
+/* GET /register */
+router.get('/register', (req, res, next) => {
+    res.send('GET /register');
+});
+
+/* POST /register */
+router.post('/register', (req, res, next) => {
+    res.send('POST /register');
+});
+
+/* GET /login */
+router.get('/login', (req, res, next) => {
+    res.send('GET /login');
+});
+
+/* POST /login */
+router.post('/login', (req, res, next) => {
+    res.send('POST /login');
+});
+
+/* GET /profile */
+router.get('/profile', (req, res, next) => {
+    res.send('GET /profile');
+});
+
+/* PUT /profile/:user_id */
+router.put('/profile/:user_id', (req, res, next) => {
+    res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot */
+router.get('/forgot', (req, res, next) => {
+    res.send('GET /forgot');
+});
+
+/* PUT /forgot */
+router.put('/forgot', (req, res, next) => {
+    res.send('PUT /forgot');
+});
+
+/* GET /reset */
+router.get('/reset/:token', (req, res, next) => {
+    res.send('GET /reset/:token');
+});
+
+/* PUT /reset */
+router.put('/reset/:token', (req, res, next) => {
+    res.send('PUT /reset/:token');
 });
 
 module.exports = router;

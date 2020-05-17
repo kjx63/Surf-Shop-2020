c5027b50216567d1908d8192bc590481340ed459
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-17 22:31:04 +0900

    Add Reviews -Edit/Update

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -23,7 +23,9 @@ module.exports = {
 
     // Reviews Update
     async reviewUpdate(req, res, next) {
-
+        await Review.findByIdAndUpdate(req.params.review_id, req.body.review);
+        req.session.success = 'Review updated successfully!'
+        res.redirect(`/posts/${req.params.id}`);
     },
     // Reviews Destroy
     async reviewDestroy(req, res, next) {

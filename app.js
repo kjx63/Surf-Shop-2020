0eb7d65444d2a416065721dea663e13ed29793f9
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-17 18:05:28 +0900

    Add reviewCreate in a controller to create a review, assign it to the post, and show in the show.ejs

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const reviews = require('./routes/reviews');
 const app = express();
 
 // connect to the database
-mongoose.connect('mongodb://localhost:27017/surf-shop2020-mapbox', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect('mongodb://localhost:27017/surf-shop2020', { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => {
--- models/review.js
+++ models/review.js
@@ -2,11 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ReviewSchema = new Schema({
-	body: String,
-	author: {
-		type: Schema.Types.ObjectId,
-		ref: 'User'
-	}
+    body: String,
+    rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    }
 });
 
 module.exports = mongoose.model('Review', ReviewSchema);

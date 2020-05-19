3f40015f5515f7c970440e9e6b60cc384d5e944d
kjx63 <ueyama.kenji.63@gmail.com>
2020-05-19 10:07:12 +0900

    Add Cluster Maps

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const mapBoxToken = process.env.MAPBOX_TOKEN;
 const mbxGeocoding = require('@mapbox/mapbox-sdk/services/geocoding');
 const geocodingClient = mbxGeocoding({ accessToken: process.env.MAPBOX_TOKEN });
 const cloudinary = require('cloudinary');
@@ -13,10 +14,11 @@ module.exports = {
     async postIndex(req, res, next) {
         let posts = await Post.paginate({}, {
             page: req.query.page || 1,
-            limit: 10
+            limit: 10,
+            sort: '-_id'
         });
         posts.page = Number(posts.page);
-        res.render('posts/index', { posts, title: 'Posts Index' });
+        res.render('posts/index', { posts, mapBoxToken, title: 'Posts Index' });
     },
     // Posts New
     postNew(req, res, next) {
@@ -38,8 +40,10 @@ module.exports = {
                 limit: 1
             })
             .send();
-        req.body.post.coordinates = response.body.features[0].geometry.coordinates;
-        let post = await Post.create(req.body.post);
+        req.body.post.geometry = response.body.features[0].geometry;
+        let post = new Post(req.body.post);
+        post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
+        await post.save();
         req.session.success = 'Post created successfully!'
         res.redirect(`/posts/${post.id}`);
     },
@@ -106,13 +110,14 @@ module.exports = {
                     limit: 1
                 })
                 .send();
-            post.coordinates = response.body.features[0].geometry.coordinates;
+            post.geometry = response.body.features[0].geometry;
             post.location = req.body.post.location;
         }
         // update the post with any new properties
         post.title = req.body.post.title;
         post.description = req.body.post.description;
         post.price = req.body.post.price;
+        post.properties.description = `<strong><a href="/posts/${post._id}">${post.title}</a></strong><p>${post.location}</p><p>${post.description.substring(0, 20)}...</p>`;
         // save the updated post into the db
         post.save();
         // redirect to show page
